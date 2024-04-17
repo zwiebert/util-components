7623c7a3e9d159b395f9bdd8fe9ecee2793e9099
zwiebert <bertw@online.de>
2024-04-17 21:15:47 +0200

    Decrease dependencies for component

--- components/utils_debug/include/debug/dbg.h
+++ components/utils_debug/include/debug/dbg.h
@@ -1,46 +1,20 @@
+#pragma once
 
+#include "log.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
 
 #ifdef __cplusplus
-
-
-
-
 extern "C++" {
-
-#include <cstdlib>
-#include <unistd.h>
-
   template<typename T>
     T rando(T low_limit, T high_limit) {
       return low_limit + static_cast<T>(rand()) / (static_cast<T>(RAND_MAX / (high_limit - low_limit)));
    }
 #endif
-#pragma once
-
-
-#include "txtio/inout.h"
 
 extern int unit_testing;
 #define UNIT_TEST (unit_testing+0)
-
-#include <assert.h>
-
-#if defined MCU_ESP32 && defined DEBUG
-#define db_check_heap()   assert(heap_caps_check_integrity_all(true))
-#else
-#define db_check_heap()
-#endif
-
-#ifdef MCU_ESP32
-#include <esp_heap_caps.h>
-#include <esp32/rom/ets_sys.h>
-#include <esp_log.h>
-#define db_puts(s) ets_printf("%s\n",s)
-#define db_printf ets_printf
-#else
-#define db_printf con_printf
-#define db_puts(s) db_printf("%s\n",s)
-#endif
 
 void abort_precond(const char *msg, int line);
 void abort_postcond(const char *msg, int line);
@@ -56,29 +30,6 @@ void abort_postcond(const char *msg, int line);
 #else
 #define postcond(x) assert(x)
 #endif
-
-#define dbg_trace()
-
-#ifndef dbg_printf
-#define dbg_printf ets_printf
-#endif
-#ifndef dbg_trace
-#define dbg_trace()   dbg_printf("trace: %s()\n", __func__)
-#endif
-
-#ifndef DISTRIBUTION
-#define dbg_dbd(x) do { if (TXTIO_IS_VERBOSE(vrbDebug)) { x; } } while(0)
-#else
-#define dbg_dbd(x)
-#endif
-
-#define if_verbose_do(x) do { if (TXTIO_IS_VERBOSE(vrbDebug)) { x; } } while(0)
-
-
-#define dbg_vpf(x) do { if (TXTIO_IS_VERBOSE(vrbDebug)) { x; } } while(0)
-
-
-
 
 #ifdef __cplusplus
   }
