c7a2686e849eeb34e463ae3bc4750d1073524b15
zwiebert <bertw@online.de>
2024-04-25 15:20:48 +0200

    Improve doc

--- components/utils_time/include/utils_time/ut_sleep.h
+++ components/utils_time/include/utils_time/ut_sleep.h
@@ -3,12 +3,12 @@
 
 /**
  * \brief       Sleep function w/o actual sleeping used in unit tests
- * \param secs  Will be added to results of any calls to \link run_time_s \endlink and \link run_time_ts \endlink
+ * \param secs  Will be added to results of any calls to @ref run_time_s  and @ref run_time_ts 
  */
 void ut_sleep(unsigned secs);
 
 /**
  * \brief        Sleep function w/o actual sleeping used in unit tests
- * \param tsecs  s/10 will be added to results of any calls to \link run_time_s \endlink and \link run_time_ts \endlink
+ * \param tsecs  s/10 will be added to results of any calls to @ref run_time_s  and @ref run_time_ts 
  */
 void ut_sleep_ts(unsigned tsecs);
