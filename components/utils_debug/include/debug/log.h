aff946ef60e9eeb32350e9077194b88611f5e4b5
zwiebert <bertw@online.de>
2024-05-03 13:15:13 +0200

    Add timestamp to log

--- components/utils_debug/include/debug/log.h
+++ components/utils_debug/include/debug/log.h
@@ -1,14 +1,26 @@
 #ifdef MCU_ESP32
+
 #include <esp_log.h>
 #define db_loge  ESP_LOGE
 #define db_logi  ESP_LOGI
 #define db_logw  ESP_LOGW
 #define db_logd  ESP_LOGD
 #define db_printf(format, ... ) ESP_LOGD("debug", format __VA_OPT__(,)  __VA_ARGS__)
+
 #else
+
+#ifdef __cplusplus
+extern "C++" {
+#endif
+const char *db_log_tm();
+#ifdef __cplusplus
+}
+#endif
+
 #include <stdio.h>
-#define db_loge( tag, format, ... ) fprintf(stderr,  "E %s: " format "\n", tag __VA_OPT__(,) __VA_ARGS__)
-#define db_logi( tag, format, ... ) fprintf(stderr,  "I %s: " format "\n", tag __VA_OPT__(,) __VA_ARGS__)
-#define db_logw( tag, format, ... ) fprintf(stderr,  "W %s: " format "\n", tag __VA_OPT__(,) __VA_ARGS__)
+#define db_loge( tag, format, ... ) fprintf(stderr,  "E%s %s: " format "\n", db_log_tm(), tag __VA_OPT__(,) __VA_ARGS__)
+#define db_logi( tag, format, ... ) fprintf(stderr,  "I%s %s: " format "\n", db_log_tm(), tag __VA_OPT__(,) __VA_ARGS__)
+#define db_logw( tag, format, ... ) fprintf(stderr,  "W%s %s: " format "\n", db_log_tm(), tag __VA_OPT__(,) __VA_ARGS__)
 #define db_printf(format, ... ) fprintf(stderr,  format  __VA_OPT__(,) __VA_ARGS__)
+
 #endif
--- components/utils_debug/src/dbg.cc
+++ components/utils_debug/src/dbg.cc
@@ -6,4 +6,16 @@
  */
 
 #include "debug/dbg.h"
+#include "debug/log.h"
 
+#include <ctime>
+
+const char *db_log_tm() {
+ static char buf[16];
+ time_t t = time(0);
+ struct tm *ltm = localtime(&t);
+ if (!ltm)
+   return "";
+ strftime(buf, sizeof buf - 1, " (%H:%M:%S)", ltm);
+ return buf;
+}
